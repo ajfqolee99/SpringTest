75b8b0a41bbdcf37bfe8903aae9077b79646949e
DESKTOP-JOSMGD6\HY <made1999@naver.com>
2024-02-06 21:51:11 +0900

    이하영 - @Controller 어노테이션 추가

--- src/main/java/com/ajfqolee/spring/test/database/StoreController.java
+++ src/main/java/com/ajfqolee/spring/test/database/StoreController.java
@@ -3,12 +3,14 @@ package com.ajfqolee.spring.test.database;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ajfqolee.spring.test.database.domain.Store;
 import com.ajfqolee.spring.test.database.service.StoreService;
 
+@Controller
 public class StoreController {
 	
 	@Autowired
